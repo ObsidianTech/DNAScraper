9195d626161f63162f8159b37c8e2feaeb5c1033
Chris Smith <chris.smith@smiledirectclub.com>
2021-06-21 09:29:50 -0500

    Save state.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
+  <Nuxt/>
 </template>
 
 <script>
 export default {
-  data () {
-  },
+  // data () {
+  // },
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template>
+  <div class="homeWrapper">
+    <h1 class="welcome">
+      DNA Lcoation Scrapper
+    </h1>
+    <h3 class="instructions">
+      Enter a city or location into the search bar, and any new results will appear below.
+    </h3>
+    <input v-model="searchInput" type="text" class="search">
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    const searchInput = "";
+    return {
+      searchInput,
+    };
+  },
 };
 </script>
